d629d8e95ff76e861cad9931aba539e3b6a8419c
Ido Ben-Yair <idoby@wix.com>
2016-01-28 11:55:07 +0200

    Removed white portion at bottom of SVG file.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,24 +9,6 @@ import source from 'vinyl-source-stream';
 const $ = gulpLoadPlugins();
 const reload = browserSync.reload;
 
-function lint(files, options) {
-  return () => {
-    return gulp.src(files)
-      .pipe(reload({stream: true, once: true}))
-      .pipe($.eslint(options))
-      .pipe($.eslint.format())
-      .pipe($.if(!browserSync.active, $.eslint.failAfterError()));
-  };
-}
-const testLintOptions = {
-  env: {
-    mocha: true
-  }
-};
-
-gulp.task('lint', lint('src/scripts/**/*.js'));
-gulp.task('lint:test', lint('test/spec/**/*.js', testLintOptions));
-
 gulp.task('html', [], () => {
   const assets = $.useref.assets({searchPath: ['.']});
 
@@ -55,26 +37,7 @@ gulp.task('extras', () => {
 
 gulp.task('clean', del.bind(null, ['.tmp', 'demo']));
 
-gulp.task('serve', [], () => {
-  browserSync({
-    notify: false,
-    port: 9000,
-    server: {
-      baseDir: ['.tmp', '.'],
-      routes: {
-        '/bower_components': 'bower_components'
-      }
-    }
-  });
-
-  gulp.watch([
-    '*.html',
-    '*.svg',
-    'src/scripts/**/*.js'
-  ]).on('change', reload);
-});
-
-gulp.task('serve:demo', () => {
+gulp.task('demo', ['build'], () => {
   browserSync({
     notify: false,
     port: 9000,
@@ -82,23 +45,6 @@ gulp.task('serve:demo', () => {
       baseDir: ['demo']
     }
   });
-});
-
-gulp.task('serve:test', () => {
-  browserSync({
-    notify: false,
-    port: 9000,
-    ui: false,
-    server: {
-      baseDir: 'test',
-      routes: {
-        '/bower_components': 'bower_components'
-      }
-    }
-  });
-
-  gulp.watch('test/spec/**/*.js').on('change', reload);
-  gulp.watch('test/spec/**/*.js', ['lint:test']);
 });
 
 gulp.task('build', ['scripts', 'html', 'extras'], () => {
